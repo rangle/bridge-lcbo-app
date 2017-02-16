e91d0990c937521cc2db7b5e78bb23e6520b21f5
Alice Mao <alice@brainboxlabs.com>
2017-02-16 18:31:30 -0500

    added store container

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Router, Route, browserHistory} from 'react-router';
 import Home from '../containers/home';
-function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+import Store from '../containers/store-container';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      	<Router history={browserHistory}>
+      		<Route path='/' component={Home}> 
+      			<Route path='store' component={Store}/>
+      		</Route> 
+      	</Router> 
+      </div>
+    );
+  }
 }
 
 
