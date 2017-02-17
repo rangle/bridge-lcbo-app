3d44eedf555c2827fdde53a1dcd547da1b0db120
jkolbe <joannakolbe@gmail.com>
2017-02-16 19:29:24 -0500

    [#139890725] - store map component (#7)
    * set up with react-google maps library. real struggle...
    
    * set up stores container and storemap component
    
    * settip up stores with redux
    
    * get stores around set location that are accessible via wheelchair and put markers on a map
    
    * change bounds to center
    
    * re-pull stores on map reposition
    
    * trying to get the marker pop-ups to work
    
    * reducer
    
    * marker pop-up

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
-import DrinkReducer from './selected-drink';
+import StoreReducer from './stores';
 
 const rootReducer = combineReducers({
-  drink: DrinkReducer,
-  form: formReducer,
+  stores: StoreReducer,
   routing: routerReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
---
+++ src/reducers/stores.js
@@ -0,0 +1,34 @@
+import {ACTION_TYPES} from '../actions/stores';
+
+const INITIAL_STATE = {
+  name: 'Basil Haydens',
+  lat: 43.669,
+  lon: -79.379,
+  test: 'pfff',
+  stores: [],
+};
+
+export default function(state = INITIAL_STATE, {type, payload}) {
+  switch (type) {
+  case ACTION_TYPES.setStore:
+    return {...state, ...{test: payload.test}};
+  case ACTION_TYPES.getStores:
+    return {...state, ...{stores: payload.stores}};
+  case ACTION_TYPES.handleBoundsChanged:
+    return {...state, ...{lon: payload.lon, lat: payload.lat}};
+  case ACTION_TYPES.handleMapMounted:
+    return {...state, ...{map: payload.map}};
+  case ACTION_TYPES.handleMarkerClick:
+    const newstores = state.stores.map(store => (
+      store.id === payload.marker.id ? Object.assign({}, store, {showInfo: true}) : store
+    ));
+    return {...state, ...{stores: newstores}};
+  case ACTION_TYPES.handleMarkerClose:
+    const newstores2 = state.stores.map(store => (
+      store.id === payload.marker.id ? Object.assign({}, store, {showInfo: false}) : store
+    ));
+    return {...state, ...{stores: newstores2}};
+  default:
+    return state;
+  }
+}
