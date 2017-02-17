671dd2e12411c8c61fc56b806bd1ff0ee6105260
Alexx Swanson <alexx@alexxswanson.com>
2017-02-16 20:18:05 -0500

    hotfix broken store page

--- src/containers/store-container.js
+++ src/containers/store-container.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-export default class StoreContainer extends Component {
-
-asdfasd
+class StoreContainer extends Component {
+  render() {
+    return <div><h1>test</h1></div>;
+  }
 }
 
-
-export default App;
+export default StoreContainer;
